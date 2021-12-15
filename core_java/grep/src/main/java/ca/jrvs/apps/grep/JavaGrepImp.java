ffdee9890e78c794c6974bffc47e1bc1046fcd14
smosma252 <osama_o.s@hotmail.com>
2021-12-15 12:26:59 -0500

    added configure

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.security.auth.login.Configuration;
+import org.apache.log4j.BasicConfigurator;
 
 public class JavaGrepImp implements JavaGrep{
 
@@ -171,7 +172,7 @@ public class JavaGrepImp implements JavaGrep{
       throw new IllegalArgumentException("USAGE: JavaGrep regex rootPath outFile");
     }
 
-
+    BasicConfigurator.configure();
 
     JavaGrepImp grepImp = new JavaGrepImp();
     grepImp.setRegex(args[0]);
